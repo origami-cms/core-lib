1e76a21509c2c542739dbcb0b013ff0af0cfab1d
Tristan Matthias <hello@tristanmatthias.com>
2018-05-09 10:27:34 +1000

    Allowed env variables to override origami config

--- src/configFile.ts
+++ src/configFile.ts
@@ -3,6 +3,7 @@ import {promisify} from 'util';
 import path from 'path';
 import {Origami} from './types';
 import {requireKeys, error} from '.';
+import dot from 'dot-object';
 
 const fsReadFile = promisify(readFile);
 const fsWriteFile = promisify(writeFile);
@@ -12,19 +13,47 @@ const CONFIG_FILE = (): string => path.resolve(process.env.CLI_CWD || './', '.or
 
 
 export namespace config {
+
     /**
-     * Attempt to load the .origami file at the current directory
+     * Converts origami_x_y envrionment variables to an object
+     * @returns {Object} Object of origami environment variables
+    */
+    export const env = (obj = {}): object => {
+        const e = Object.entries(process.env)
+            .filter(([key, value]) => /^origami_.*$/.test(key))
+            .map(([key, value]) => [
+                key
+                    // Replace _ with .
+                    .replace(/_/g, '.')
+                    // Remove preceding 'origami.'
+                    .split('.').slice(1).join('.'),
+                value
+            ])
+            .forEach(([key, value]) => {
+                dot.str(key as string, value, obj);
+            });
+
+        return obj;
+    };
+
+    /**
+     * Attempt to load the .origami file at the current directory. Overwrites with any ENV variables
      * @returns {Origami.Config|Boolean} The Origami file as json, or false if it cannot be
      * found or loaded correctly
     */
     export const read = async (): Promise<Origami.Config | false> => {
+        let c = {} as Origami.Config;
         try {
-            return JSON.parse(
+            c = JSON.parse(
                 (await fsReadFile(CONFIG_FILE())).toString()
             );
         } catch (e) {
             return false;
         }
+
+        c = env(c) as Origami.Config;
+
+        return c;
     };
 
 
@@ -32,39 +61,39 @@ export namespace config {
      * Override/write the .origami file
      * @param file JSON config for Origami app to override
      */
-    export const write = async(file: Origami.Config): Promise<void> => {
-        const TAB_SIZE = 4;
-        return fsWriteFile(
+export const write = async(file: Origami.Config): Promise<void> => {
+    const TAB_SIZE = 4;
+    return fsWriteFile(
             CONFIG_FILE(),
             JSON.stringify(file, null, TAB_SIZE)
         );
-    };
+};
 
-    export const validate = (config: Origami.Config) => {
-        try {
-            requireKeys([
-                'store'
-            ], config);
-        } catch (e) {
-            return error(new Error(`Origami: Missing '${e.key}' setting`));
-        }
+export const validate = (config: Origami.Config) => {
+    try {
+        requireKeys([
+            'store'
+        ], config);
+    } catch (e) {
+        return error(new Error(`Origami: Missing '${e.key}' setting`));
+    }
 
 
         // ------------------------------------------------------ Validate store
-        const store = `origami-store-${config.store.type}`;
-        try {
-            require(path.resolve(process.cwd(), 'node_modules', store));
-        } catch (e) {
-            if (e.name === 'Error') {
-                return error(
+    const store = `origami-store-${config.store.type}`;
+    try {
+        require(path.resolve(process.cwd(), 'node_modules', store));
+    } catch (e) {
+        if (e.name === 'Error') {
+            return error(
                     new Error(
                         `Origami: Could not find store plugin '${store}'. Try running 'yarn install ${store}'`
                     )
                 );
-            }
-
-            return error(e);
         }
-    };
+
+        return error(e);
+    }
+};
 
 }
